80c201104b4f84510a00213d9118effcf6c7ea40
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-06-11 18:53:51 +0200

    Ajout du module morgan

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var server = express();
+var morgan = require("morgan");
+
+server.use(morgan("dev"));
 
 server.listen(3000, function() {
     console.log("Le serveur écoute sur le port 3000")
