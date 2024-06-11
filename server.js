0a9defa7a3f9669c8ef1902839e4612ed5aed184
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-06-11 18:47:47 +0200

    first commmit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var server = express();
+
+server.listen(3000, function() {
+    console.log("Le serveur écoute sur le port 3000")
+});
+
+server.get("/", (req, res) => {
+    console.log("Page d'accueil demandée")
+    res.end("Page d'accueil demandée")
+});
+
+server.post("/test", (req, res) => {
+    console.log("Demande reçue avec la méthode POST sur l'url /test")
+    res.end("Demande POST reçue")
+});
