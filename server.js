2a66a7cf701f59b4c584f54f57d692015f76fe01
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-06-12 10:27:36 +0200

    Ajout de CSS personnalisé

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var server = express();
 var morgan = require("morgan");
 var router = require("./router");
 
+server.use(express.static("public"));
 server.use(morgan("dev"));
 server.use("/", router);
 
